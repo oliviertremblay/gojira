f580071bbd2b0019a0fb3e665fe99afd35cf6ea7
Olivier Tremblay <olivier.tremblay@thezombie.net>
2013-10-31 19:37:33 -0400

    README + some mods

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 	"strings"
-	"encoding/json"
-	"fmt"
 )
 
 func main() {
@@ -15,7 +16,6 @@ func main() {
 		s, err := rdr.ReadString('\n')
 		js = append(js, s)
 		if err != nil {
-			fmt.Println(err)
 			break
 		}
 
@@ -23,11 +23,32 @@ func main() {
 	njs := strings.Join(js, "")
 	var obj interface{}
 	err := json.Unmarshal([]byte(njs), &obj)
-	if err != nil { panic(err)}
-	m := obj.(map[string]interface{})
-	issues := m["issues"].([]interface{})
-	for _, v := range issues{
-		i := v.(map[string]interface{})
-		fmt.Println(fmt.Sprintf("%s (%s): %s",i["key"].(string),i["fields"].(map[string]interface{})["issuetype"].(map[string]interface{})["name"].(string), i["fields"].(map[string]interface{})["summary"].(string)))
+	if err != nil {
+		panic(err)
 	}
+	issues, _ := jsonWalker("issues", obj)
+	issuesSlice := issues.([]interface{})
+	for _, v := range issuesSlice {
+		key, _ := jsonWalker("key", v)
+		issuetype,_ := jsonWalker("fields/issuetype/name",v)
+		summary, _ := jsonWalker("fields/summary",v)
+		fmt.Println(fmt.Sprintf("%s (%s): %s", key.(string), issuetype.(string), summary.(string)))
+	}
+}
+
+func jsonWalker(path string, json interface{}) (interface{}, error) {
+	p := strings.Split(path, "/")
+	tmpval := json
+	for i, subpath := range p {
+		submap, ok := tmpval.(map[string]interface{})
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("Bad path, %s is not a map[string]interface{}", p[i-1]))
+		}
+		if i < (len(p) - 1) {
+			tmpval = submap[subpath]
+		} else {
+			return submap[subpath], nil
+		}
+	}
+	return nil, errors.New("Woooops")
 }
