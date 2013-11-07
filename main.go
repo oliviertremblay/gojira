bedde1f1e440880188694f0302cba59a77afc335
Olivier Tremblay <olivier.tremblay@thezombie.net>
2013-11-07 08:34:40 -0500

    Integrated http query for task listing, a few options

--- main.go
+++ main.go
@@ -7,11 +7,37 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"github.com/jessevdk/go-flags"
+	"net/http"
+	"io"
+	"crypto/tls"
 )
 
+var Options struct {
+    CurrentSprint bool `short:"c" long:"current-sprint" description:"Show stories for current sprint"`
+	User string `short:"u" long:"user" description:"Your username"`
+	Passwd string `short:"p" long:"pass" description:"Your password"`
+	NoCheckSSL bool `short:"n" long:"no-check-ssl" description:"Don't check ssl validity"`
+}
+
 func main() {
+	flags.Parse(&Options)
 	js := make([]string, 0)
-	rdr := bufio.NewReader(os.Stdin)
+	var input io.Reader
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: Options.NoCheckSSL},
+	}
+    client := &http.Client{Transport: tr}
+	if Options.CurrentSprint {
+		resp, err := client.Get(fmt.Sprintf("https://%s:%s@jira.gammae.com/rest/api/2/search?jql=sprint+in+openSprints()", Options.User, Options.Passwd))
+		if err != nil{
+			panic(err)
+		}
+		input = resp.Body
+	} else {
+		input = os.Stdin
+	}
+	rdr := bufio.NewReader(input)
 	for {
 		s, err := rdr.ReadString('\n')
 		js = append(js, s)
@@ -32,7 +58,16 @@ func main() {
 		key, _ := jsonWalker("key", v)
 		issuetype,_ := jsonWalker("fields/issuetype/name",v)
 		summary, _ := jsonWalker("fields/summary",v)
-		fmt.Println(fmt.Sprintf("%s (%s): %s", key.(string), issuetype.(string), summary.(string)))
+		parentJS, err := jsonWalker("fields/parent/key", v)
+		var parent string
+		parent, _ = parentJS.(string)
+		if err != nil {
+			parent = ""
+		}
+		if parent != "" {
+			parent = fmt.Sprintf(" of %s", parent)
+		}
+		fmt.Println(fmt.Sprintf("%s (%s%s): %s", key.(string), issuetype.(string), parent, summary.(string)))
 	}
 }
 
