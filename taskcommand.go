f92b1261946ae468db60307b86a5588c3863dffe
Olivier Tremblay <olivier.tremblay@thezombie.net>
2014-08-24 05:05:32 -0400

    Now with proper assign command

--- taskcommand.go
+++ taskcommand.go
@@ -25,34 +25,67 @@ func (tc *TaskCommand) Execute(args []string) error {
 	if err != nil {
 		return err
 	}
-	switch args[1] {
-	case "grab":
-		err = iss.Assign(options.User, jc)
-		if err != nil {
-			return err
-		}
-	case "start":
-		err = iss.StartProgress(jc)
-		if err != nil {
-			return err
-		}
-	case "stop":
-		err = iss.StopProgress(jc)
-		if err != nil {
-			return err
-		}
-	case "resolve":
-		if len(args) > 2 {
-			err := iss.ResolveIssue(jc, strings.Join(args[2:], " "))
-			if err != nil {
-				return err
-			}
-		} else {
-			fmt.Println("Possible values for resolution:")
-			p, _ := iss.PossibleResolutions(jc)
-			fmt.Print(p)
-		}
+
+	var m map[string]interface{} = nil
+
+	if len(args) > 2 {
+		m = map[string]interface{}{"resolution": map[string]interface{}{"name": capitalize(args[2:])}}
 	}
 
+	err = iss.TaskTransition(jc, args[1], m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func capitalize(str []string) string {
+	str2 := []string{}
+	for _, substr := range str {
+		str2 = append(str2, strings.Split(substr, " ")...)
+	}
+	for i, substr := range str2 {
+		cap := false
+		s2 := ""
+		for _, k := range substr {
+			if !cap {
+				s2 += strings.ToUpper(string(k))
+				cap = true
+			} else {
+				s2 += strings.ToLower(string(k))
+			}
+
+		}
+		str2[i] = s2
+	}
+	return strings.Join(str, " ")
+}
+
+type AssignCommand struct{}
+
+var assignCommand AssignCommand
+
+func init() {
+	parser.AddCommand("assign", "Assign tasks",
+		`gojira assign TASK-1234 TASK-1235 TASK-1236 someone
+
+Allows you to assign tasks to people.
+`, &assignCommand)
+}
+
+func (tc *AssignCommand) Execute(args []string) error {
+	if len(args) != 2 {
+		fmt.Errorf("Wrong number of arguments. Usage: gojira assign TASK-1234 username")
+	}
+	jc := libgojira.NewJiraClient(options)
+	iss, err := jc.GetIssue(args[0])
+	if err != nil {
+		return err
+	}
+	err = iss.Assign(args[1], jc)
+	if err != nil {
+		return err
+	}
 	return nil
 }
